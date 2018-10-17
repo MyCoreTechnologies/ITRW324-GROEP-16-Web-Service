63cd3198d2cccda155483a1f638099b6a31928e7
Gerhard van Wyk <21091137@student.g.nwu.ac.za>
2018-10-18 00:26:21 +0200

    Update to the date and promise formation

--- api/routes/book.js
+++ api/routes/book.js
@@ -40,7 +40,6 @@ var transporter = nodemailer.createTransport({
 //                book_isbn_13_number
 //                book_price
 //                book_type
-//                date_placed
 //                author_name
 //                subject_code
 try{
@@ -51,7 +50,7 @@ try{
         var iauthor = 'INSERT INTO author set ?';
         var ibookauthor = 'INSERT INTO book_author set ?';
         var ibooksubject = 'INSERT INTO book_subject set ?';
-        var fbook = 'SELECT * FROM book WHERE Book_Name = ? AND Book_Edition = ? AND Book_ISBN_10_Number = ? AND Book_ISBN_13_Number = ? AND Book_Price = ? AND Book_Type = ? AND Date_Placed = ? AND STUDENT_Student_Number = ?';
+        var fbook = 'SELECT * FROM book WHERE Book_Name = ? AND Book_Edition = ? AND Book_ISBN_10_Number = ? AND Book_ISBN_13_Number = ? AND Book_Price = ? AND Book_Type = ? AND STUDENT_Student_Number = ?';
         var fauthor = 'SELECT * FROM author WHERE Author_Name = ?';
         var fsubject = 'SELECT * FROM subject WHERE Subject_Code = ?';
         var fbookauthor = 'SELECT * FROM book_author WHERE BOOK_Book_Number = ? AND AUTHOR_Author_Number = ?';
@@ -71,7 +70,6 @@ try{
             Book_ISBN_13_Number:        req.body.book_isbn_13_number,
             Book_Price:                 req.body.book_price,
             Book_Type:                  req.body.book_type,
-            Date_Placed:                req.body.date_placed,
             STUDENT_Student_Number:     req.keystudentNumber
         };
         
@@ -84,8 +82,24 @@ try{
         pool.getConnection()
         .then(function(conn) {
             connection = conn;
+
             //Sending a SQL query to the database and making a PROMISE
-            return connection.query(fbook, [req.body.book_name, req.body.book_edition, req.body.book_isbn_10_number, req.body.book_isbn_13_number, req.body.book_price, req.body.book_type, req.body.date_placed, req.keystudentNumber]);
+            return connection.query(fsubject, req.body.subject_code);
+        })
+        .then(function(subjectRow){
+            //If function to test if the data you want to find exists in the database
+            if(subjectRow[0].length === 0)
+            {
+                res.status(400).json({message:'Subject does not exist.'});
+            } else{
+                if(subjectRow[0].Subject_Code === req.body.subject_code)
+                {
+                    subject_number = subjectRow[0].Subject_Number;
+                    console.log('Subject read.');
+                }
+            }
+            //Sending a SQL query to the database and making a PROMISE
+            return connection.query(fbook, [req.body.book_name, req.body.book_edition, req.body.book_isbn_10_number, req.body.book_isbn_13_number, req.body.book_price, req.body.book_type, 'DEFAULT', req.keystudentNumber]);
         })
         .then(function(bookRow){
             //If function to test if the data you want to add exists in the database
@@ -102,7 +116,7 @@ try{
                 });
             }
             //Sending a SQL query to the database and making a PROMISE
-            return connection.query(fbook, [req.body.book_name, req.body.book_edition, req.body.book_isbn_10_number, req.body.book_isbn_13_number, req.body.book_price, req.body.book_type, req.body.date_placed, req.keystudentNumber]);
+            return connection.query(fbook, [req.body.book_name, req.body.book_edition, req.body.book_isbn_10_number, req.body.book_isbn_13_number, req.body.book_price, req.body.book_type, req.keystudentNumber]);
         })
         .then(function(booksRow){
             //If function to test if the data you want to find exists in the database
@@ -114,23 +128,8 @@ try{
                 console.log('Book read.');
             }
             
-            //Sending a SQL query to the database and making a PROMISE
-            return connection.query(fsubject, req.body.subject_code);
-        })
-        .then(function(subjectRow){
-            //If function to test if the data you want to find exists in the database
-            if(subjectRow[0].length === 0)
-            {
-                res.status(400).json({message:'Subject does not exist.'});
-            } else{
-                if(subjectRow[0].Subject_Code === req.body.subject_code)
-                {
-                    subject_number = subjectRow[0].Subject_Number;
-                    console.log('Subject read.');
-                }
             //Sending a SQL query to the database and making a PROMISE    
             return connection.query(fauthor, req.body.author_name);
-            }
         })
         .then(function(authorRow){
             //If function to test if the data you want to add exists in the database
